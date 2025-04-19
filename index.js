5f39606712acfeabe6d4f59904b5b2bf11dd67e5
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-04-19 19:13:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'public', 'static')));
 
 
 const storage = multer.diskStorage({
